0efe1595d258d04e21f7f8c92975ade5b379eeb3
Christopher Adeogun <112475290+oluwatobi2018@users.noreply.github.com>
2022-09-21 22:14:24 +0100

    Update Ntisa Mobile Dev Assessment.java

--- Ntisa Mobile Dev Assessment.java
+++ Ntisa Mobile Dev Assessment.java
@@ -2,7 +2,24 @@
     package="Food menu">
     <manifest xmlns:android="http://schemas.android.com/apk/res/android"
     package="com.skewerspot.ss_menu">
-
+<?xml version="1.0" encoding="UTF-8"?>
+<module type="JAVA_MODULE" version="4">
+  <component name="NewModuleRootManager" inherit-compiler-output="true">
+    <exclude-output />
+    <content url="file://$MODULE_DIR$">
+      <sourceFolder url="file://$MODULE_DIR$/lib" isTestSource="false" />
+      <sourceFolder url="file://$MODULE_DIR$/test" isTestSource="true" />
+      <excludeFolder url="file://$MODULE_DIR$/.dart_tool" />
+      <excludeFolder url="file://$MODULE_DIR$/.idea" />
+      <excludeFolder url="file://$MODULE_DIR$/.pub" />
+      <excludeFolder url="file://$MODULE_DIR$/build" />
+    </content>
+    <orderEntry type="sourceFolder" forTests="false" />
+    <orderEntry type="library" name="Dart SDK" level="project" />
+    <orderEntry type="library" name="Flutter Plugins" level="project" />
+    <orderEntry type="library" name="Dart Packages" level="project" />
+  </component>
+</module>
     <!-- io.flutter.app.FlutterApplication is an android.app.Application that
          calls FlutterMain.startInitialization(this); in its onCreate method.
          In most cases you can leave this as-is, but you if you want to provide
